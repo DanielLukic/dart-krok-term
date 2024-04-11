0993915e1e8e572006c71e736afc63682115a0e2
Daniel Lukic <daniel.lukic@gmx.net>
2024-04-11 18:32:14 +0200

    Use arrow keys for scrolling chart

--- lib/src/krok_term/feature/chart/chart_keys.dart
+++ lib/src/krok_term/feature/chart/chart_keys.dart
@@ -7,14 +7,18 @@ extension on Window {
         action: () => _refresh.value = DateTime.timestamp());
 
     onKey('<S-h>', //
+        aliases: ['<S-Left>'],
         description: 'Jump left',
         action: () => _projection.scrollBy(10));
     onKey('<S-l>',
+        aliases: ['<S-Right>'],
         description: 'Jump right', action: () => _projection.scrollBy(-10));
     onKey('h', //
+        aliases: ['<Left>'],
         description: 'Scroll left',
         action: () => _projection.scrollBy(2));
     onKey('l', //
+        aliases: ['<Right>'],
         description: 'Scroll right',
         action: () => _projection.scrollBy(-2));
 
@@ -24,14 +28,10 @@ extension on Window {
       _interval.value = OhlcInterval.values[change];
     }
 
-    onKey('<Left>', //
-        aliases: ['i'],
-        description: 'Smaller interval',
-        action: () => changeInterval(-1));
-    onKey('<Right>', //
-        aliases: ['<S-i>'],
-        description: 'Bigger interval',
-        action: () => changeInterval(1));
+    onKey('i',
+        description: 'Smaller interval', action: () => changeInterval(-1));
+    onKey('<S-i>',
+        description: 'Bigger interval', action: () => changeInterval(1));
 
     onKey('+', description: 'Zoom out', action: () => _projection.zoomBy(-1));
     onKey('-', description: 'Zoom in', action: () => _projection.zoomBy(1));
