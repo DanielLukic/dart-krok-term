38d563da74e652e08653ce6acf02617be6897d32
Daniel Lukic <daniel.lukic@gmx.net>
2024-04-18 18:00:35 +0200

    Trigger alert notifications when ticker price crosses alert price

--- lib/src/krok_term/feature/alerting.dart
+++ lib/src/krok_term/feature/alerting.dart
@@ -1,11 +1,38 @@
 import 'package:krok_term/src/krok_term/core/krok_core.dart';
 import 'package:krok_term/src/krok_term/repository/krok_repos.dart';
+import 'package:krok_term/src/krok_term/repository/ticker_repo.dart';
+import 'package:rxdart/rxdart.dart';
 
 import '../repository/alerts_repo.dart';
-import '../repository/ticker_repo.dart';
 
 class Alerting {
-  Alerting(TickersRepo tickersRepo);
+  Alerting() {
+    combine([alerts, tickers]).debounceTime(1.seconds).listen((event) {
+      final alerts = event[0] as Alerts;
+      final tickers = event[1] as Tickers;
+
+      final fired = <AlertData>[];
+      for (final entry in alerts.entries) {
+        final ticks = tickers[entry.key];
+        if (ticks == null) continue;
+
+        for (final alert in entry.value) {
+          if (alert.mode == 'above' && ticks.last > alert.price) {
+            desktop.sendMessage(AlertTriggered(alert, ticks.last));
+            fired.add(alert);
+          }
+          if (alert.mode == 'below' && ticks.last < alert.price) {
+            desktop.sendMessage(AlertTriggered(alert, ticks.last));
+            fired.add(alert);
+          }
+        }
+      }
+
+      for (final it in fired) {
+        alertsRepo.remove(it);
+      }
+    });
+  }
 
   void onAdd(AlertAdd add) {
     final lastPrice = add.pair.price(add.lastPrice);
