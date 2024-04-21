cf70b6be5a4874db03c690bbf7fe1be9f2728982
Daniel Lukic <daniel.lukic@gmx.net>
2024-04-21 09:37:51 +0200

    Remove unused function

--- lib/src/krok_term/common/functions.dart
+++ lib/src/krok_term/common/functions.dart
@@ -2,8 +2,5 @@ import 'extensions.dart';
 
 String joinPath(List<String> elements) => elements.joinPath();
 
-List<dynamic> listIn(dynamic it, String key) =>
-    (it as Map<String, dynamic>)[key] as List<dynamic>;
-
 List<(A, B)> zip<A, B>(List<A> a, List<B> b) =>
     [for (int i = 0; i < a.length; i++) (a[i], b[i])];
